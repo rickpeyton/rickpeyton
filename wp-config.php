a1e1326b3b433c6c88623d3825ae388cf38638b3
Rick Peyton <peytorb@gmail.com>
2019-02-16 12:47:22 -0600

    Swap HTTPS back

--- wp-config.php
+++ wp-config.php
@@ -82,7 +82,7 @@ define('WP_DEBUG', getenv('WORDPRESS_DEBUG'));
 // If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
 // see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-	$_SERVER['HTTPS'] = 'off';
+	$_SERVER['HTTPS'] = 'on';
 }
 
 /* That's all, stop editing! Happy blogging. */
